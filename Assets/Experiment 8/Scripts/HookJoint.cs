88bd84b37aa8a3251bf7b6e3a113c8e6c6bb29ee
Meelouth <justtelejka@yandex.ru>
2020-02-02 16:25:13 +0300

    Added weights for exp 8

---
+++ Assets/Experiment 8/Scripts/HookJoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class HookJoint : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hook"))
+            Joint(other.gameObject);
+            
+    }
+
+    private void Joint(GameObject to)
+    {
+        HingeJoint joint = to.AddComponent<HingeJoint>();
+        InitializeJoint(joint);
+    }
+
+    private void InitializeJoint(HingeJoint joint)
+    {
+        joint.connectedBody = GetComponent<Rigidbody>();
+    }
+}
