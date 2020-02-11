16700f0bc16317d14b2964b58a7a00f53dd58a66
Meelouth <justtelejka@yandex.ru>
2020-02-11 18:07:38 +0300

    Water molecules attraction

---
+++ Assets/Experiment 4 new/Scripts/Attractable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Experiment_4_new.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Attractable : MonoBehaviour, IAttractable
+    {
+        private Rigidbody _rigidbody;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        
+        public Rigidbody GetRigidbody()
+        {
+            return _rigidbody;
+        }
+    }
+}
---
+++ Assets/Experiment 4 new/Scripts/AttractionByWaterMolecules.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Experiment_4_new.Scripts;
+using UnityEngine;
+
+public class AttractionByWaterMolecules : MonoBehaviour
+{
+    [SerializeField] private float pullForce = 1f;
+    [SerializeField] private float attractTime = 2f;
+    [SerializeField] private Vector3 attractionDirection = Vector3.down;
+
+    private HingeJoint _hingeJoint;
+
+    [SerializeField] private float breakForce = 800f;
+
+    private void OnTriggerExit(Collider other)
+    {
+        IAttractable attractable = other.GetComponent<IAttractable>(); //in ontriggerenter?
+        
+        if (attractable == null)
+            return;
+
+        Attract(attractable.GetRigidbody());
+    }
+
+    private void Attract(Rigidbody attractable)
+    {
+        _hingeJoint = CreateHingeJoint(attractable);
+    }
+
+    private HingeJoint CreateHingeJoint(Rigidbody connectedBody)
+    {
+        var createdHingeJoint = gameObject.AddComponent<HingeJoint>();
+
+        createdHingeJoint.connectedBody = connectedBody;
+        createdHingeJoint.breakForce = breakForce;
+
+        return createdHingeJoint;
+    }
+}
