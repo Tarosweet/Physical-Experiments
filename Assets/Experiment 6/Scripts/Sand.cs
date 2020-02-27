365f7bd2e2363fb42f3faa710e0b780af5193392
Meelouth <justtelejka@yandex.ru>
2020-02-27 17:05:16 +0300

    Sand fixes

--- Assets/Experiment 6/Scripts/Sand.cs
+++ Assets/Experiment 6/Scripts/Sand.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(Collider))]
 public class Sand : MonoBehaviour
 {
 
