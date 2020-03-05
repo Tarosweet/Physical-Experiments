bcc307e443906838fcc6bc7e32bed31c4281e881
Meelouth <justtelejka@yandex.ru>
2020-03-05 16:07:13 +0300

    ar test

---
+++ Assets/Scripts/Testing/ARScale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARScale : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Transform ar;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var value = slider.value;
+        ar.localScale = new Vector3(value,value,value);
+    }
+}
