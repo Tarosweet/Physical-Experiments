26ffc3699b1ec597c67374e6acd4713fa0743572
Meelouth <justtelejka@yandex.ru>
2020-03-10 15:50:47 +0300

    Merge

---
+++ Assets/Scripts/TestSystem/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TestSystem
+{
+    public class Test : MonoBehaviour
+    {
+        public Answer correctAnswer;
+
+        [SerializeField] private UnityEvent onGiveCorrectAnswer;
+        [SerializeField] private UnityEvent onGiveIncorrectAnswer;
+        
+        public void GiveAnAnswer(Answer answer)
+        {
+            if (answer.CheckAnswer())
+            {
+                onGiveCorrectAnswer?.Invoke();
+                Debug.Log("Correct!");
+                return;
+            }
+            
+            onGiveIncorrectAnswer?.Invoke();
+        }
+    }
+}
