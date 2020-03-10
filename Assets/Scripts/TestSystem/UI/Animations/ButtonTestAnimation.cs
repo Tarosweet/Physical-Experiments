e1cc5fbb6a88dac15d62385e89f4d66f77b9036f
Meelouth <justtelejka@yandex.ru>
2020-03-10 18:12:07 +0300

    Chose one from 4 test and write answer test

---
+++ Assets/Scripts/TestSystem/UI/Animations/ButtonTestAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestSystem.UI.Animations
+{
+    public class ButtonTestAnimation : MonoBehaviour
+    {
+        [SerializeField] private Image buttonImage;
+
+        [SerializeField] private ButtonsTestAnimations buttonsTestAnimations;
+
+        public void SetColor(Color color)
+        {
+            buttonImage.color = color;
+        }
+
+        public void OnClick()
+        {
+            buttonsTestAnimations.clickedButton = this;
+        }
+    }
+}
---
+++ Assets/Scripts/TestSystem/UI/Animations/ButtonsTestAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestSystem.UI.Animations
+{
+    public class ButtonsTestAnimations : MonoBehaviour
+    {
+        [SerializeField] private Color correctButtonColor = Color.green;
+        [SerializeField] private Color incorrectButtonColor = Color.red;
+        [SerializeField] private Color defaultButtonColor = Color.white;
+        
+        [SerializeField] private List<ButtonTestAnimation> buttonTestAnimations;
+
+        public ButtonTestAnimation clickedButton;
+
+        public void SetColors(bool isCorrect)
+        {
+            foreach (var button in buttonTestAnimations)
+            {
+                if (button == clickedButton)
+                {
+                    button.SetColor(isCorrect ? correctButtonColor : incorrectButtonColor);
+                }
+                else
+                {
+                    button.SetColor(defaultButtonColor);
+                }
+            }
+        }
+    }
+}
