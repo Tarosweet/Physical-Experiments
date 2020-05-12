2ff83caffa25c6f9d7d30b22439a5421079a4547
Meelouth <justtelejka@yandex.ru>
2020-05-13 01:28:35 +0300

    fixes

---
+++ Assets/Experiment_1/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Experiment_1
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private Transform rotatingTransform;
+        [SerializeField] private float angle = 20f;
+
+        private void OnMouseDrag()
+        {
+            Debug.Log("reo");
+            var x = Input.GetAxis ("Mouse X") * angle * Mathf.Deg2Rad;
+        
+            rotatingTransform.Rotate(Vector3.forward, x * Time.deltaTime);
+        }
+    }
+}
