bfddae0b26ebe192a679e389d3e1be64d8a77bcb
Meelouth <justtelejka@yandex.ru>
2020-05-12 16:06:10 +0300

    Ready to build

--- Assets/Scripts/SceneManagement/SceneLoader.cs
+++ Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -12,18 +14,38 @@ namespace SceneManagement
         
         private Scene _currentScene;
 
+        private Scene _mainScene;
+
+        private void Start()
+        {
+            _mainScene = SceneManager.GetActiveScene();
+        }
+
         public void LoadScene(int id)
         {
+            SceneManager.sceneLoaded += SceneManagerOnSceneLoaded;
             SceneManager.LoadScene(id, LoadSceneMode.Additive);
             _currentScene = SceneManager.GetSceneByBuildIndex(id);
+            
             onLoadScene?.Invoke();
+        }
+
+        private void SceneManagerOnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            SceneManager.SetActiveScene(scene);
         }
 
         public void DestroyCurrentAdditiveScene()
         {
+            SceneManager.sceneUnloaded += SceneManagerOnSceneUnloaded;
             DestroyScene(_currentScene.buildIndex);
         }
-        
+
+        private void SceneManagerOnSceneUnloaded(Scene scene)
+        {
+            SceneManager.SetActiveScene(_mainScene);
+        }
+
         public void DestroyScene(int id)
         {
             SceneManager.UnloadSceneAsync(id);
